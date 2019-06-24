3e3815f6a0bbe368a87599772d2a59e95ad2fb76
PlaidAlise <37463139+PlaidAlise@users.noreply.github.com>
2019-06-24 09:57:35 +0800

    Add files via upload

---
+++ exl.py
@@ -0,0 +1,7 @@
+print("Hello World!")
+print("Hello again.")
+print("I Like typing this.")
+print("This is fun.")
+print('Yay!printing.')
+print("I'd much rather you 'not'.")
+print('I"said"do not touch this.')
